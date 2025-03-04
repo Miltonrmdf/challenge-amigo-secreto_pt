5457d16256ec268d6b0c6735d7b9826027b08816
Miltonrmdf <miltonrmdf@gmail.com>
2025-03-04 01:38:15 -0300

    Começando o arquivo app.js (amanha eu termino se Deus quiser, ta muito tardegit status)

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+
+function textoNaTela(tag,texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML;
+}
+
+textoNaTela('p',)
+
+
+function cadastroNome (){
+    let nome = document.querySelector('input').value;
+    if(value=''){
+        prompt('Favor preencher o campo')
+    }
+}
+
